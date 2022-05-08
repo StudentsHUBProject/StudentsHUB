cf617b69f4e3c79ab3693687bf8fcd37bf56bac2
TheDevil0fHK <lorenzoralli999@gmail.com>
2022-05-08 16:40:15 +0200

    Corsi: aggiunta nav con filtri di ricerca

---
+++ src/assets/js/corsi.js
@@ -0,0 +1,10 @@
+function DropMenu(){
+    let list = document.querySelector('.list');
+    list.classList.toggle('newlist');
+}
+
+function DropMenu2(){
+    let list = document.querySelector('.list1');
+    list.classList.toggle('newlist');
+}
+
